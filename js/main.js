9f19d302e2c19278496e0d5a5594976794cfb20d
slobodanmiric08051977 <slobamiric@gmail.com>
2020-12-18 21:13:54 +0100

    slider resen

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
         responsive:{
             0:{
                 items:1
